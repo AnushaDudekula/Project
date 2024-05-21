fa0d58fbe8dda4d003f840729b92c02f519260e7
AnushaDudekula <anushadudekula1999@gmail.com>
2024-05-21 11:55:44 +0530

    test

---
+++ framework/src/test/java/frameworkTest/ContactTest.java
@@ -0,0 +1,15 @@
+package frameworkTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createTest() {
+		System.out.println("anusha");
+	}
+	@Test
+	public void createconTest() {
+	System.out.println("anusha");	
+	}
+
+}
