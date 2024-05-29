84d1056c285efa022fc6c815753296b968733aab
ChinniAnu <anushadudekula09@gmail.com>
2024-05-29 21:40:58 +0530

    class created

---
+++ framework/src/test/java/frameworkTest/OrgconTest.java
@@ -0,0 +1,14 @@
+package frameworkTest;
+
+import org.testng.annotations.Test;
+
+public class OrgconTest {
+	@Test
+	public void createorgTest() {
+		System.out.println("anusha");
+	}
+	@Test
+	public void createorgphTest() {
+	System.out.println("anusha");	
+	}
+}
