f9054126fa5751c8bb28a615b937da9b9d07cd38
AnushaDudekula <anushadudekula1999@gmail.com>
2024-05-29 12:39:24 +0530

    class created

---
+++ framework/src/test/java/frameworkTest/monuTest.java
@@ -0,0 +1,15 @@
+package frameworkTest;
+
+import org.testng.annotations.Test;
+
+public class monuTest {
+	@Test
+	public void createorgTest() {
+		System.out.println("anusha");
+	}
+	@Test
+	public void createorgphTest() {
+	System.out.println("anusha");	
+	}
+
+}
