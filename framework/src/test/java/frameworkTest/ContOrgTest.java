ce8d6dde2a1448d755d4f66b3138936defb119a0
AnushaDudekula <anushadudekula1999@gmail.com>
2024-05-29 11:25:28 +0530

    new Class Added

---
+++ framework/src/test/java/frameworkTest/ContOrgTest.java
@@ -0,0 +1,14 @@
+package frameworkTest;
+
+import org.testng.annotations.Test;
+
+public class ContOrgTest {
+	@Test
+	public void createconTest() {
+		System.out.println("anusha");
+	}
+	@Test
+	public void createconorgTest() {
+	System.out.println("anusha");	
+	}
+}
