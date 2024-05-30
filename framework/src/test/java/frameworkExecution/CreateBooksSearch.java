c1bdc7217a98dbbd2abcb3d2193e93d491aa0239
DELL <DELL@DESKTOP-07J22P1>
2024-05-30 14:58:26 +0530

    new package and one class created.

---
+++ framework/src/test/java/frameworkExecution/CreateBooksSearch.java
@@ -0,0 +1,13 @@
+package frameworkExecution;
+
+import org.testng.annotations.Test;
+
+@org.testng.annotations.Test
+
+public class CreateBooksSearch {
+	@Test
+	public void searchBooks() {
+		System.out.println("Books search Test case is created");
+	}
+
+}
